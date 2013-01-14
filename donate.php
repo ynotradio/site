5f1bf1bbad0343ce341415f08b3fbae50f8470ae
Alan Deitch <alandeitch@gmail.com>
2013-01-13 19:23:12 -0600

    remove extra donate form

--- donate.php
+++ donate.php
@@ -13,15 +13,7 @@ require ("ext/custom_text_fns.php");
   <div class="nine columns content">
     <h1></h1>
     <h1>Donate to Y-Not Radio</h1>
-    <p>
     <?php display_custom_text(1); ?>
-    <p>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" class="center">
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-      <input type="hidden" name="cmd" value="_s-xclick">
-      <input type="hidden" name="hosted_button_id" value="NSC3MPELFXKUJ">
-    </form>
   </div>
   <div class="three columns"><?php require ("featured_concerts_and_ads.php") ?></div>
 </div> <!-- end of row div -->
