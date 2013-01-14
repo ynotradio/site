182f1e94453cb3f4bdec12f1a0139aab5e193dc5
Alan Deitch <alandeitch@gmail.com>
2013-01-13 20:08:22 -0600

    mobile

---
+++ mobile.php
@@ -0,0 +1,28 @@
+<?php
+
+$page_file = "mobile.php";
+$page_title = "Mobile";
+
+require ("ext/main_fns.php");
+require ("ext/header.php");
+
+/*----- CONTENT ------*/
+?>
+<div class="row">
+  <div class="nine columns content">
+    <h1></h1>
+    <h1>Mobile</h1>
+    Many of you have been asking how you can listen to Y-Not Radio on your iPhones, Androids, Blackberries, etc... and we now have two geat options for you. <strong>Once you get started, you can text your requests to 267-293-YNOT (9668).</strong>
+    <p>
+    <a href="http://www.live365.com/web/components/content/downloads/mobile.live" target="_blank"><img src="http://ynotradio.net/images/live365app.jpg" alt="Live 365" width="100" border="0" align="left" style="margin: 0px 10px 0px 0px;" title=""></a>
+     Y-Not Radio streams 24/7 and now you can tune in for free on your <strong>iPhone</strong>, <strong>iPad</strong>, or <strong>iPod Touch</strong> with the new <em>Live 365 v. 2.0 iPhone App</em>. This app will allow you to multi-task with Y-Not Radio playing in the background. You can download the app from <a href="http://www.live365.com/web/components/content/downloads/mobile.live" target="_blank">Live 365</a> or directly from the <a href="http://itunes.apple.com/app/live365-radio/id301645072?mt=8" target="_blank">iTunes App Store</a>. After downloading and launching the app, just search for Y-Not Radio (spelled just like that) and then make us a preset for easy access every time. <em>The Live 365 App will soon be available for <strong>Android</strong> as well.</em>
+    <p>
+    <a href="http://tunein.com/mobile/" target="_blank"><img src="http://ynotradio.net/images/tunein_radio_logo.jpg" alt="TuneIn Radio" width="100" border="0" align="left" style="margin: 0px 10px 0px 0px;" title=""></a>
+    Y-Not Radio is also available through the free <em>TuneIn Radio App</em> on <strong>iPhone</strong>, <strong>Android</strong>, <strong>Blackberry</strong>, <strong>Palm</strong>, and <strong>Windows Phone</strong>. Find your phone and download the app <a href="http://tunein.com/mobile/" target="_blank">here</a>. After downloading and launching the TuneIn Radio,  search for Y-Not Radio (spelling counts) and then make us a preset for easy access every time.
+    <div class="footnote">
+      *As with any streaming media, charges may be applied from your mobile phone provider depending on your data plan.
+    </div>
+  </div>
+  <div class="three columns"><?php require ("featured_concerts_and_ads.php") ?></div>
+</div> <!-- end of row div -->
+<?php require ("ext/footer.php"); ?>
