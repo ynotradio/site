f1db4af9ac8e47f4d46a137c86a05edfcb15be9c
Alan Deitch <alandeitch@gmail.com>
2013-01-23 14:19:23 -0600

    copy change

--- aboutus.php
+++ aboutus.php
@@ -11,8 +11,13 @@ require ("partials/_header.php");
 <div class="row">
   <div class="nine columns content">
     <h1>About Us</h1>
-    <h3>We're Still Alive!</h3>
-    Hello friends! Thank you for your outpouring of support over the last few weeks, especially during my show last Thursday. You all told me how much you'd miss me and the other DJs who were leaving; you said that you knew it was the people who made that station what it was, and that you'd love to hear us start up again somewhere else soon. Well, soon is now and somewhere else is here - the all new Y-Not Radio! The name has changed but our mission remains the same as it always has, whether we've been in a professional radio station or a DIY Bunker studio, to bring you the best alternative, modern, and indie rock! We've had our share of obstacles recently, but they have only made us a stronger, more unified team who will do whatever it takes to share in the joy of music with you. Thank you for finding us here, thank you for sticking with the people rather than the name, and thank you for listening! We hope that you enjoy Y-Not Radio, and please please please tell your friends!
+    <b>Y-Not Radio</b> is the home of indie rock in Philadelphia.  Our roots go back to Philly's long-running alternative FM station <b>Y100</b> (WPLY), where a new music show called Y-Not aired every sunday night from 1997 through the station's end in 2005.  After the demise of Y100, program director <b>Jim McGuinn</b>, promotions director <b>Josh T. Landow</b>, and host <b>Joey O.</b> (amongst others) kept the spirit of the station alive online as <b>Y100 Rocks</b> (later <b>Y-Rock</b>).  As of 2010, the station returned to its independent roots, morphing once again into Y-Not Radio with Landow at the helm.  Through all the name changes, our mission has always remained the same: to bring you the best alternative, modern, and indie rock! Thank you for finding us, thank you for listening, and thank you for your support! We hope that you enjoy Y-Not Radio!
+    <div class="center">
+      <a href="contact.php"><h3>Contact Us</h3></a>
+      <a href="deejays.php"><h3>Y-Not Radio Staff</h3></a>
+      <a href="donate.php"><h3>Support Y-Not Radio</h3></a>
+      <a href="ymail.php"><h3>Subscribe To Y-Not's Newsletter</h3></a>
+    </div>
   </div>
   <div class="three columns"><?php require ("partials/_featured_concerts_and_ads.php") ?></div>
 </div> <!-- end of row div -->
