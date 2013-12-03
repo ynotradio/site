7ebd6efbb4d872a357468dd96e51fe5aa0d748f4
Alan Deitch <alandeitch@gmail.com>
2013-12-03 08:56:17 -0600

    link back to year end poll page

--- yearendstaffpicks.php
+++ yearendstaffpicks.php
@@ -11,9 +11,11 @@ require ("partials/_header.php");
 ?>
 <div class="row">
   <div class="twelve columns content">
-    <div class="center top-spacer_20 bottom-spacer_20"><img src="images/yearend2013_banner.jpg" alt="YNot Year End Poll 2013" width="900px"></div>
+    <div class="center top-spacer_20"><img src="images/yearend2013_banner.jpg" alt="YNot Year End Poll 2013" width="900px"></div>
+    <div class="text-right right-spacer_30"><a href="yearendpoll.php" >Go Back To Year End Poll</a></div>
     <h1><?php echo date('Y');?> Y-Not Staff Favorites</h1>
     <?php show_year_end_staff_picks(); ?>
+    <div><a href="yearendpoll.php" >Go Back To Year End Poll</a></div>
   </div>
 </div> <!-- end of row div -->
 <?php require ("partials/_footer.php"); ?>
