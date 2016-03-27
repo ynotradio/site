08ef1e35fe31dcae104859dd02028c151d851967
TJ Nicolaides <tj.nicolaides@thinkbrownstone.com>
2016-03-27 12:48:41 -0400

    Adjusting the twitter share button to have a better CTA ("tune in now" vs. "Check out") and the #modernrockmadness hash tag

--- madness.php
+++ madness.php
@@ -21,8 +21,8 @@ $band_id = $_POST['band_id'];
     <div id="mrm_text">
   Download your Modern Rock Madness <?php echo date('Y'); ?> brackets <a href="http://ynotradio.opendrive.com/files/98800526_19BFV_7dd0/YNot_MRM2015_Bracket.pdf">here</a> and listen all week as Y-Not bands go head to head! Help your favorites advance to the next round by voting here, or if you're listening on the go, you can text your votes in to 267-293-YNOT.  Plus get more color commentary on each day's matches by visiting our partner site <a href=" http://tristateindie.com/modern-rock-madness-2015" target="_new">Tri State Indie</a>.
       <div class="social">
-        <a href="https://twitter.com/share" class="twitter-share-button" data-text="Check out @YNotRadio's Modern Rock Madness - 64 bands go head to head www.ynotradio.net/madness #YNotMadness" data-count="none" data-via="YNotRadio">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
-        <div class="fb-like" data-href="http://www.ynotradio.net/madness.php" data-send="true" data-width="450" data-show-faces="false"></div>
+        <a href="https://twitter.com/share" class="twitter-share-button" data-text="Tune in now to @YNotRadio's Modern Rock Madness - 64 bands go head to head! #modernrockmadness" data-count="none" data-via="YNotRadio">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
+        <div class="fb-like" data-href="http://www.ynotradio.net/madness.php?2016" data-send="true" data-width="450" data-show-faces="false"></div>
       </div>
     </div>
 <?php
