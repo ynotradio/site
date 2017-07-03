4e44408a5268cb84018c482e1be8abb3e881e8c8
TJ Nicolaides <tj.nicolaides@thinkbrownstone.com>
2017-07-02 23:11:51 -0400

    Changing deploy step

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,4 +5,4 @@ set -e
 sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
-lftp -c "set ftps:initial-prot ''; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+lftp -c "set ftps:initial-prot ''; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv . .; quit;"
