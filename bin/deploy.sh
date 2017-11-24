6d82ab00951430446b028eca4b25c8d0245e572f
TJ Nicolaides <tj.nicolaides@thinkbrownstone.com>
2017-11-24 11:48:44 -0500

    ..

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@
 # compile
 npm run build
 # sync on staging
-rsync --rsync-path="sudo -u www-data rsync" -az -vv --no-p --no-g --chmod=ugo=rwX -O -e 'ssh -p 22' ~/clone/site/themes bitnami@54.84.246.119:~/apps/beta.ynotradio.net/htdocs/wp-content/themes/ynotradio
+rsync --rsync-path="sudo -u www-data rsync" -az -vv --no-p --no-g --chmod=ugo=rwX -O -e 'ssh -p 22' ~/clone/site/theme bitnami@54.84.246.119:~/apps/beta.ynotradio.net/htdocs/wp-content/themes/ynotradio
