fac83e4487b4ab5e5d27200a94c664bc9e7fac62
TJ Nicolaides <tj.nicolaides@thinkbrownstone.com>
2017-11-25 23:32:47 -0500

    check for unique posts before inserting new CDOTW entries

--- site/theme/migrations/migration-2.0.0.php
+++ site/theme/migrations/migration-2.0.0.php
@@ -13,7 +13,7 @@ class Migrate_2_0_0
     public function __construct($wpdb)
     {
         $this->wpdb = $wpdb;
-        include('config-2.0.0.php');
+        include 'config-2.0.0.php';
     }
 
     public function run()
@@ -27,11 +27,52 @@ class Migrate_2_0_0
         echo $message . '<br />';
     }
 
-    private function getCDOTW() {
+    private function getCDOTW()
+    {
         $rows = $this->migration_db->get_results("select * from cdotw");
-        foreach ($rows as $obj) :
+        foreach ($rows as $obj):
             $this->log($obj->title);
+            if ($this->checkForUniqueCDOTW($obj->id)) {
+                $this->addCDOTW($obj);
+            };
         endforeach;
+    }
+
+    private function checkForUniqueCDOTW($id)
+    {
+        $args = array(
+            'post_type'   => 'cd_of_the_week',
+            'post_status' => 'any',
+            'meta_query'  => array(
+                array(
+                    'key'     => '_cdotw_legacy_id',
+                    'value'   => $id,
+                    'compare' => '=',
+                ),
+            ),
+        );
+        $query = new WP_Query($args);
+        return $query->found_posts <= 0;
+    }
+
+    private function addCDOTW($obj)
+    {
+        wp_insert_post(array(
+            'post_title'   => $obj->title,
+            'post_type'    => 'cd_of_the_week',
+            'post_status' => 'publish',
+            'post_date' => $obj->date,
+            'post_content' => $obj->review,
+            'meta_input'   => array(
+                '_cdotw_artist'     => $obj->artist,
+                '_cdotw_artist_url' => $obj->band,
+                '_cdotw_legacy_id'  => $obj->id,
+                '_cdotw_label'      => $obj->label,
+                '_cdotw_reviewer'   => $obj->reviewer,
+                '_cdotw_pic_url'    => $obj->cd_pic_url,
+                '_cdotw_date'       => $obj->date,
+            ),
+        ));
     }
 }
 
