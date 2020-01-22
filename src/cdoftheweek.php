a6cbe2c25886800d1e249dbe1c1b871ca643d27d
TJ Nicolaides <tj.nicolaides@gmail.com>
2020-01-22 10:03:46 -0500

    Fix for error on cd of the week page

--- src/cdoftheweek.php
+++ src/cdoftheweek.php
@@ -7,12 +7,14 @@ require ("functions/main_fns.php");
 require ("functions/cdotw_fns.php");
 require ("partials/_header.php");
 
+$cd_id = isset($_GET['id']) ? $_GET['id'] : null;
+
 /*----- CONTENT ------*/
 ?>
 <div class="row">
   <div class="nine columns">
     <h1>CD of The Week</h1>
-    <?php cdotw($_GET['id']); ?>
+    <?php cdotw($cd_id); ?>
   </div>
   <div class="three columns"><?php require ("partials/_featured_concerts_and_ads.php") ?></div>
 </div> <!-- end of row div -->
