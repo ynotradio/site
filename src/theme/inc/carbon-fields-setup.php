17bb4c6b6dba1e5ea0e47c33998ca4c4ce3a1e17
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-03-24 09:03:31 -0400

    think we got composer running to install carbon fields

--- src/theme/inc/carbon-fields-setup.php
+++ src/theme/inc/carbon-fields-setup.php
@@ -4,11 +4,14 @@ use Carbon_Fields\Field;
 
 add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
 function crb_attach_theme_options() {
-
-    Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
+    Container::make( 'theme_options', __( 'Theme Options' ) )
         ->add_fields( array(
             Field::make( 'text', 'crb_text', 'Text Field' ),
-            Field::make( 'image', 'crb_employee_photo', 'Photo' )
-    ->set_value_type( 'url' )
         ) );
+}
+
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+    require_once( ABSPATH . '/vendor/autoload.php' );
+    \Carbon_Fields\Carbon_Fields::boot();
 }
