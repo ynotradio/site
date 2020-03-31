4d6306528872df02a2cd7eeb78424fab9689bfb1
TJ Nicolaides <tj.nicolaides@airbnb.com>
2020-03-31 01:54:56 -0400

    Add Artist custom fields

--- src/theme/inc/post-types/artist.php
+++ src/theme/inc/post-types/artist.php
@@ -13,7 +13,45 @@ function create_post_type__artist()
         'has_archive' => true,
         'rewrite' => array('slug' => 'artist'),
         'menu_icon' => 'dashicons-art',
+        'supports' => array('title'),
     ));
 }
 
 add_action('init', 'create_post_type__artist');
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+function crb_register__artist()
+{
+    Container::make('post_meta', 'Artist Information')
+        ->where('post_type', '=', 'artist')
+        ->add_fields(array(
+            Field::make('text', 'crb_artist__url', 'URL'),
+            Field::make('text', 'crb_artist__picture', 'Picture (Imported)'),
+        ));
+
+    Container::make('post_meta', 'Social Links')
+        ->where('post_type', '=', 'artist')
+        ->add_fields(array(
+            Field::make('complex', 'crb_social_urls', 'Social Links')
+                ->add_fields(array(
+                    Field::make('text', 'label', 'Label')
+                        ->set_width(50) // condense layout so field takes only 50% of the available width
+                        ->set_required(),
+                    Field::make('text', 'url', 'URL')
+                        ->set_width(50)
+                        ->set_required(),
+                )
+                ),
+        ));
+    Container::make('post_meta', 'Modern Rock Madness')
+        ->where('post_type', '=', 'artist')
+        ->add_fields(array(
+            Field::make('text', 'crb_artist__abbreviation', 'Abbreviation')
+                ->set_width(10),
+        ));
+
+}
+
+add_action('carbon_fields_register_fields', 'crb_register__artist');
