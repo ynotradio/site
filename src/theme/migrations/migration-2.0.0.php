7c3ef1b41ba35853787d4eefeb21218d6c6ccff7
TJ Nicolaides <tj.nicolaides@airbnb.com>
2021-03-27 22:52:32 -0400

    Add custom texts import

--- src/theme/migrations/migration-2.0.0.php
+++ src/theme/migrations/migration-2.0.0.php
@@ -8,7 +8,7 @@ global $wpdb;
 class Migrate_2_0_0 extends Migration
 {
 
-    private $wpdb;
+    private $__wpdb;
 
     public function __construct($wpdb)
     {
@@ -20,30 +20,39 @@ class Migrate_2_0_0 extends Migration
     public function run()
     {
         $this->log('Running migration to 2.0.0...');
-        $this->getCDOTW();
+        $this->__importCDOTW();
+        $this->__importCustomText();
     }
 
-    private function getCDOTW()
+    /**
+     * CDOTW
+     */
+
+    private function __importCDOTW()
     {
+        $this->log("****************************************");
+        $this->log("Importing CD of the Week");
         $rows = $this->migration_db->get_results("SELECT * from cdotw");
+        $this->log("Found " . count($rows) . " cdotw rows to import");
 
         foreach ($rows as $obj):
             $this->log($obj->title);
-            if ($this->checkForUniqueCDOTW($obj->id)) {
-                $this->addCDOTW($obj);
+            if ($this->__checkForUniqueCDOTW($obj->id)) {
+                $this->__addCDOTW($obj);
             };
         endforeach;
+        $this->log("****************************************");
     }
 
-    private function checkForUniqueCDOTW($id)
+    private function __checkForUniqueCDOTW($id)
     {
         $args = array(
-            'post_type'   => 'cd_of_the_week',
+            'post_type' => 'cd_of_the_week',
             'post_status' => 'any',
-            'meta_query'  => array(
+            'meta_query' => array(
                 array(
-                    'key'     => '_cdotw_legacy_id',
-                    'value'   => $id,
+                    'key' => '_cdotw_legacy_id',
+                    'value' => $id,
                     'compare' => '=',
                 ),
             ),
@@ -52,25 +61,78 @@ class Migrate_2_0_0 extends Migration
         return $query->found_posts <= 0;
     }
 
-    private function addCDOTW($obj)
+    private function __addCDOTW($obj)
     {
         wp_insert_post(array(
-            'post_title'   => $obj->title,
-            'post_type'    => 'cd_of_the_week',
+            'post_title' => $obj->title,
+            'post_type' => 'cd_of_the_week',
             'post_status' => 'publish',
             'post_date' => $obj->date,
             'post_content' => $obj->review,
-            'meta_input'   => array(
-                '_cdotw_artist'     => $obj->artist,
+            'meta_input' => array(
+                '_cdotw_artist' => $obj->artist,
                 '_cdotw_artist_url' => $obj->band,
-                '_cdotw_legacy_id'  => $obj->id,
-                '_cdotw_label'      => $obj->label,
-                '_cdotw_reviewer'   => $obj->reviewer,
-                '_cdotw_pic_url'    => $obj->cd_pic_url,
-                '_cdotw_date'       => $obj->date,
+                '_cdotw_legacy_id' => $obj->id,
+                '_cdotw_label' => $obj->label,
+                '_cdotw_reviewer' => $obj->reviewer,
+                '_cdotw_pic_url' => $obj->cd_pic_url,
+                '_cdotw_date' => $obj->date,
             ),
         ));
     }
+
+    /**
+     * Custom Texts
+     */
+
+    private function __importCustomText()
+    {
+        $this->log("****************************************");
+        $this->log("Importing Custom Texts");
+        $rows = $this->migration_db->get_results("SELECT * from custom_texts");
+        $this->log("Found " . count($rows) . " custom_texts rows to import");
+        foreach ($rows as $obj):
+            $this->log($obj->title);
+            if ($this->__checkForUniqueCustomText($obj->id)) {
+                $this->__addCustomText($obj);
+            };
+        endforeach;
+
+        $this->log("****************************************");
+    }
+
+    private function __checkForUniqueCustomText($id)
+    {
+        $args = array(
+            'post_type' => 'page',
+            'post_status' => 'any',
+            'meta_query' => array(
+                array(
+                    'key' => '_custom_texts_legacy_id',
+                    'value' => $id,
+                    'compare' => '=',
+                ),
+            ),
+        );
+        $query = new WP_Query($args);
+        return $query->found_posts <= 0;
+    }
+
+    private function __addCustomText($obj)
+    {
+        $status = $obj->status === 'active' ? 'publish' : 'draft';
+        $this->log($status);
+        wp_insert_post(array(
+            'post_title' => $obj->title,
+            'post_type' => 'page',
+            'post_status' => $obj->status === 'active' ? 'publish' : 'draft',
+            'post_content' => $obj->html,
+            'meta_input' => array(
+                '_custom_texts_legacy_id' => $obj->id,
+            ),
+        ));
+    }
+
 }
 
 $migration = new Migrate_2_0_0($wpdb);
