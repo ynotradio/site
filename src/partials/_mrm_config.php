587225b53f6a718aab6d3ffd7c5ea31684d8c934
TJ Nicolaides <tjnicolaides@gmail.com>
2025-03-23 18:16:29 +0000

    update 2025 configs

--- src/partials/_mrm_config.php
+++ src/partials/_mrm_config.php
@@ -10,7 +10,7 @@
 $madness_start_date = '2025-03-24';
 
 // Bracket PDF URL
-$madness_bracket_pdf_url = "https://od.lk/d/183444924_hXENe/MRM2024Bracket.pdf";
+$madness_bracket_pdf_url = "https://od.lk/d/219145587_ruE6q/MRM2025Bracket.pdf";
 
 // Banner image URL
-$madness_banner_image_url = "https://imgur.com/Nk246Ss.png";
+$madness_banner_image_url = "https://i.imgur.com/QfqMVsE.png";
