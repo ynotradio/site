925f2ae03e3ce2d74ba5fa107cfc4652877161e2
TJ Nicolaides <tjnicolaides@gmail.com>
2025-04-26 20:09:29 +0000

    update Auth0, handle footer errors

--- src/partials/_footer.php
+++ src/partials/_footer.php
@@ -1,20 +1,44 @@
- <?php
-require 'vendor/autoload.php';
-require '__env_loader.php';
+<?php
+// Use absolute path to vendor directory
+$root_path = $_SERVER['DOCUMENT_ROOT'];
+$vendor_autoload = $root_path . '/vendor/autoload.php';
+$env_loader = dirname(__FILE__) . '/__env_loader.php';
+
+// Check if files exist before requiring them
+if (file_exists($vendor_autoload)) {
+    require $vendor_autoload;
+} else {
+    error_log("Unable to load vendor/autoload.php from {$vendor_autoload}");
+}
+
+if (file_exists($env_loader)) {
+    require $env_loader;
+} else {
+    error_log("Unable to load __env_loader.php");
+}
 
 $uri = $_SERVER["HTTP_HOST"];
 $protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
 
-$auth0 = new Auth0\SDK\Auth0([
-    'domain' => $_ENV['AUTH0_DOMAIN'],
-    'client_id' => $_ENV['AUTH0_CLIENT_ID'],
-    'client_secret' => $_ENV['AUTH0_CLIENT_SECRET'],
-    'redirect_uri' => $protocol . "://" . $uri,
-    // The scope determines what data is provided in the ID token.
-     // See: https://auth0.com/docs/scopes/current
-     'scope' => 'openid email profile',
-]);
-$userInfo = $auth0->getUser();
+// Only initialize Auth0 if autoload was successful and required env vars are present
+$userInfo = null;
+if (class_exists('Auth0\SDK\Auth0') && 
+    !empty($_ENV['AUTH0_DOMAIN']) && 
+    !empty($_ENV['AUTH0_CLIENT_ID']) && 
+    !empty($_ENV['AUTH0_CLIENT_SECRET'])) {
+    try {
+        $auth0 = new Auth0\SDK\Auth0([
+            'domain' => $_ENV['AUTH0_DOMAIN'],
+            'client_id' => $_ENV['AUTH0_CLIENT_ID'],
+            'client_secret' => $_ENV['AUTH0_CLIENT_SECRET'],
+            'redirect_uri' => $protocol . "://" . $uri,
+            'scope' => 'openid email profile',
+        ]);
+        $userInfo = $auth0->getUser();
+    } catch (\Exception $e) {
+        error_log('Auth0 initialization error: ' . $e->getMessage());
+    }
+}
 
 ?>
   </div>
@@ -22,15 +46,15 @@ $userInfo = $auth0->getUser();
     Copyright <?php echo date('Y'); ?> Y-Not Radio
       <br>
       <a href="/aboutus.php">About Us</a> | <a href="/contact.php">Contact</a>
-      <?php
-
-if ($userInfo) {
-    ?>
-| <a href="/social_logout.php">Log out</a>
-        <?php
-}
-?>
+      <?php if ($userInfo) { ?>
+        | <a href="/social_logout.php">Log out</a>
+      <?php } ?>
   </footer>
-  <?php mysqli_close(open_db()); ?>
+  <?php 
+  // Check if function exists before calling
+  if (function_exists('open_db')) { 
+      mysqli_close(open_db()); 
+  } 
+  ?>
   </body>
 </html>
