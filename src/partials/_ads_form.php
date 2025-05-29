1f2c2c9a056ebf4cc6dfcf4ed453bed8d3957022
TJ Nicolaides <tjnicolaides@gmail.com>
2025-05-28 21:47:21 -0400

    Update ads_form

--- src/partials/_ads_form.php
+++ src/partials/_ads_form.php
@@ -1,53 +1,54 @@
 <?php
-  if ($ad['name'] == '')
-    $pic_url = $target;
-  else
-    $pic_url = $ad["pic_url"];
-
-  echo"<fieldset>
-    <div class=\"control-group\">
-      <label class=\"required\">Name</label>
-      <div class=\"control\">
-      <input type=\"text\" name=\"name\" class=\"input-l\" value=\"".$ad["name"]."\">
-      </div>
-    </div>
-    <div class=\"control-group\">
-      <label class=\"required\">Start Date</label>
-      <div class=\"control\">
-        <input type=\"text\" name=\"start_date\" class=\"date\" placeholder=\"YYYY/MM/DD\" value=\"".$ad["start_date"]."\">
-      </div>
-    </div>
-    <div class=\"control-group\">
-      <label class=\"required\">End Date</label>
-      <div class=\"control\">
-        <input type=\"text\" name=\"end_date\" class=\"date\" placeholder=\"YYYY/MM/DD\" value=\"".$ad["end_date"]."\">
-      </div>
-    </div>
-    <div class=\"control-group\">
-      <label class=\"required\">Picture Url</label>
-      <div class=\"control\">
-        <input type=\"text\" name=\"pic_url\" class=\"input-xl\" value=\"".$pic_url."\">
-      </div>
-    </div>
-    <div class=\"control-group\">
-      <label class=\"required\">Link Url</label>
-      <div class=\"control\">
-        <input type=\"text\" name=\"web_url\" class=\"input-xl\" value=\"".$ad["web_url"]."\">
-      </div>
-      <div class=\"control-group\">
-      <label class=\"required\">Priority</label>
-      <div class=\"control\">
-        <input type=\"text\" name=\"priority\" class=\"input-s\" value=\"".$ad["priority"]."\">
-      </div>
-    </div>
-    <div class=\"form-actions\">";
-      if ($ad['name'] == ''){
-        echo "<input type=\"hidden\" name=\"action\" value=\"insert\">
-              <input type=\"submit\" class=\"btn-success\" value=\"Add Ad\">";
-      } else {
-        echo "<input type=\"hidden\" name=\"action\" value=\"update\">
-              <input type=\"submit\" class=\"btn-info\" value=\"Update Ad\">";
-      }
-    echo "</div>
-  </fieldset>";
+$ad = $ad ?? [];
+$name = $ad['name'] ?? '';
+$start_date = $ad['start_date'] ?? '';
+$end_date = $ad['end_date'] ?? '';
+$pic_url = $ad['pic_url'] ?? '';
+$web_url = $ad['web_url'] ?? '';
+$priority = $ad['priority'] ?? '';
+$action = ($name === '') ? 'insert' : 'update';
+$button_class = ($action === 'insert') ? 'btn-success' : 'btn-info';
+$button_label = ($action === 'insert') ? 'Add Ad' : 'Update Ad';
 ?>
+<fieldset>
+  <div class="control-group">
+    <label class="required">Name</label>
+    <div class="control">
+      <input type="text" name="name" class="input-l" value="<?= htmlspecialchars($name) ?>">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="required">Start Date</label>
+    <div class="control">
+      <input type="text" name="start_date" class="date" placeholder="YYYY/MM/DD" value="<?= htmlspecialchars($start_date) ?>">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="required">End Date</label>
+    <div class="control">
+      <input type="text" name="end_date" class="date" placeholder="YYYY/MM/DD" value="<?= htmlspecialchars($end_date) ?>">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="required">Picture Url</label>
+    <div class="control">
+      <input type="text" name="pic_url" class="input-xl" value="<?= htmlspecialchars($pic_url) ?>">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="required">Link Url</label>
+    <div class="control">
+      <input type="text" name="web_url" class="input-xl" value="<?= htmlspecialchars($web_url) ?>">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="required">Priority</label>
+    <div class="control">
+      <input type="text" name="priority" class="input-s" value="<?= htmlspecialchars($priority) ?>">
+    </div>
+  </div>
+  <div class="form-actions">
+    <input type="hidden" name="action" value="<?= $action ?>">
+    <input type="submit" class="<?= $button_class ?>" value="<?= $button_label ?>">
+  </div>
+</fieldset>
