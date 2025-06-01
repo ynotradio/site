a8576589084839e6ec5fbc1c838b17a849c667a9
TJ Nicolaides <tjnicolaides@gmail.com>
2025-06-01 02:44:12 +0000

    rm web.config. move year_end_poll_song_picks to cp

--- src/cp/year_end_poll_song_picks.php
+++ src/cp/year_end_poll_song_picks.php
@@ -1,11 +1,11 @@
 <?php
 
-$page_file = "year_end_poll_song_picks.php.php";
+$page_file = "year_end_poll_song_picks.php";
 $page_title = "Year End Poll Contestant's Song Picks";
 
-require ("functions/main_fns.php");
-require ("functions/year_end_poll_fns.php");
-require ("partials/_header.php");
+require ("../functions/main_fns.php");
+require ("../functions/year_end_poll_fns.php");
+require ("../partials/_header.php");
 
 $contestant_id = $_GET['contestant_id'];
 
@@ -23,10 +23,10 @@ if (!$_SESSION["logged_in"]) {
     <div class="top-spacer_20">
       <a href="year_end_poll_contestants.php">View all Year End Poll Contestants</a>
       <p>
-      <a href="cp/">Control Panel</a>
+      <a href="index.php">Control Panel</a>
     </div>
   </div>
 </div> <!-- end of row div -->
 <?php }
-  require ("partials/_footer.php");
+  require ("../partials/_footer.php");
 ?>
