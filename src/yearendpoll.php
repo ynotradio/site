9664a4415b4dfe8798232ec41a4f845629c3ab4a
TJ Nicolaides <tjnicolaides@gmail.com>
2025-11-28 22:19:02 -0500

    fix yearend poll page typos

--- src/yearendpoll.php
+++ src/yearendpoll.php
@@ -88,11 +88,11 @@ $isPollActive = (time() <= $poll_end_datetime);
 
             <div class="row">
                 <div class="column twelve">
-                    <p>As the year winds down, it's time to pick all of your favorite music, movies, TV, and more from 2025! Cast your votes and you could win a pair of Apple Airpods Max headphones and the chance to play your own top 20 songs of the year on Y-Not Radio!
+                    <p>As the year winds down, it's time to pick all of your favorite music, movies, TV, and more from 2025! Cast your votes and you could win a pair of Apple AirPods Max headphones and the chance to play your own top 20 songs of the year on Y-Not Radio!
                         <br><br>
                         Vote now thru Dec. 23rd. Then we'll count down <i>The Top 225 of 2025</i> from December 29th thru January 2nd! You can sponsor a 10 song block of the countdown by making a $25 donation <a href="https://www.paypal.com/paypalme/ynotradio/25" target=_blank>here</a>.
                         <br><br>
-                        <i>Need some ideas? Check out the Y-Not DJs best of lists <a href="yearendstaffpicks.php">here</a>.</i>
+                        <i>Need some ideas? Check out the Y-Not DJs best-of lists <a href="yearendstaffpicks.php">here</a>.</i>
                     </p>
                 </div>
             </div>
