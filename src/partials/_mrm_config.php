2f2713558620eddd8200babf643af6c4926da4b3
TJ Nicolaides <tjnicolaides@gmail.com>
2026-03-10 21:54:52 -0400

    update mrm start date

--- src/partials/_mrm_config.php
+++ src/partials/_mrm_config.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
  * Modern Rock Madness Configuration
- * 
+ *
  * This file contains all the configuration variables for Modern Rock Madness.
  * Update these values each year for the new tournament.
  */
 
 // Tournament start date (YYYY-MM-DD format)
-$madness_start_date = '2025-03-24';
+$madness_start_date = '2026-03-23';
 
 // Bracket PDF URL
 $madness_bracket_pdf_url = "https://od.lk/d/219145587_ruE6q/MRM2025Bracket.pdf";
